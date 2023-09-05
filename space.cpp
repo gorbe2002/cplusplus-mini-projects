e0ab7a6659381d77bbe90ebd8ac977baa420582a
Gabriel <95147025+gorbe2002@users.noreply.github.com>
2023-09-05 19:18:26 -0400

    Update space.cpp

--- space.cpp
+++ space.cpp
@@ -6,6 +6,16 @@ Write a space.cpp program that helps him keep track of his target weight by:
   - It should ask him what his earth weight is.
   - Ask him to enter a number for the planet he wants to fight on.
   - It should then compute his weight on the destination planet.
+
+Table of Conversion:
+# 	Planet 	  Relative Gravity
+1 	Mercury 	0.38
+2 	Venus 	  0.91
+3 	Mars 	    0.38
+4 	Jupiter 	2.34
+5 	Saturn 	  1.06
+6 	Uranus 	  0.92
+7 	Neptune 	1.19
 */
 
 #include <iostream>
