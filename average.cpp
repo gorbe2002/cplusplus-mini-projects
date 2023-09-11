8110e3913b79da79ad84360eb5d6a1872fe03466
Gabriel <95147025+gorbe2002@users.noreply.github.com>
2023-09-10 23:13:40 -0400

    Create average.cpp

---
+++ average.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+// Define average() here:
+double average(double num1, double num2) {
+  return (num1 + num2) / 2;
+}
+
+int main() {
+  
+  cout << average(42.0, 24.0) << endl;
+  cout << average(1.0, 2.0) << endl;
+  
+}
