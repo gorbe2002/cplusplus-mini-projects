1f9d0a1f334846c834ce3446f329e22bfff69415
Gabriel <95147025+gorbe2002@users.noreply.github.com>
2023-09-10 23:14:57 -0400

    Create tenth_power.cpp

---
+++ tenth_power.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+// Define tenth_power() here:
+int tenth_power(int num) {
+  return pow(num, 10);
+}
+
+int main() {
+  
+  cout << tenth_power(0) << endl;
+  cout << tenth_power(1) << endl;
+  cout << tenth_power(2) << endl;
+  
+}
